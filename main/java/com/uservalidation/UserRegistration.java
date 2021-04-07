0cb416906884d2b71f3e5c993b0e63d6dcbfb5e9
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-08 00:19:20 +0530

    Welcome in User Registration

---
+++ main/java/com/uservalidation/UserRegistration.java
@@ -0,0 +1,18 @@
+package com.uservalidation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface Validation{
+    public abstract void Validation(String input);
+
+}
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to user Registration prog");
+    }
+
+}
+
+
