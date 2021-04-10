d9a151e4ef55e7e13b0d852374ed2b89f4ad3d37
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-10 09:44:29 +0530

    Mobile Validation

--- main/java/com/uservalidation/UserRegistration.java
+++ main/java/com/uservalidation/UserRegistration.java
@@ -13,29 +13,26 @@ public class UserRegistration {
     public static void main(String[] args) {
 
         System.out.println("Welcome to user Registration prog");
-        Validation vFname =(fName) ->{
+
+        Validation vFname = (fName) -> {
             Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{3,}$");
             Matcher matcher = pattern.matcher(fName);
             boolean matchFound = matcher.find();
-            if(matchFound)
-            {
+            if (matchFound) {
                 System.out.println("First Name Is Valid");
-            }
-            else {
+            } else {
                 System.out.println("First Name Is Not Valid");
             }
 
         };
         vFname.Validation("Murari");
-        Validation vLname =(lName) ->{
+        Validation vLname = (lName) -> {
             Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{3,}$");
             Matcher matcher = pattern.matcher(lName);
             boolean matchFound = matcher.find();
-            if(matchFound)
-            {
+            if (matchFound) {
                 System.out.println("Last Name Is Valid");
-            }
-            else {
+            } else {
                 System.out.println("Last Name Is Not Valid");
             }
 
@@ -43,23 +40,32 @@ public class UserRegistration {
         vLname.Validation("Kumar");
 
 
-Validation emailValidation =(email)->{
+        Validation emailValidation = (email) -> {
             String regex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(email);
             boolean matchFound = matcher.find();
-            if(matchFound)
-            {
+            if (matchFound) {
                 System.out.println("Valid email");
-            }
-            else {
+            } else {
                 System.out.println("Invalid email");
             }
         };
+
         emailValidation.Validation("murario5nitJsr2016@gmail.com");
+        Validation mobNoValidation = (mobNo) -> {
+            String regex = ("^[1-9]{2}[\\s][6-9]{1}[0-9]{9}$");
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(mobNo);
+            boolean matchFound = matcher.find();
+            if (matchFound) {
+                System.out.println("Valid mobile number");
+            } else {
+                System.out.println("Invalid mobile number");
+            }
+        };
+        mobNoValidation.Validation("8709628464");
 
     }
-
 }
-
 
